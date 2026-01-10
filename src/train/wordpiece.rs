c16ce970b6c23fc4975a2b0b6dda58d3c3c43a61
hwangfucius <junzhe.hwangfu@gmail.com>
2026-01-10 03:15:08 +0100

    feat: add WordPieceTrainer

--- src/train/wordpiece.rs
+++ src/train/wordpiece.rs
@@ -1,5 +1,7 @@
 //! WordPiece training algorithm.
 
+use crate::train::TrainingConfig;
+
 /// Represents a word split into subword units during training.
 #[derive(Debug, Clone)]
 struct Word {
@@ -7,4 +9,9 @@ struct Word {
     symbols: Vec<String>,
     /// How many times this word appears in the corpus
     count: usize,
+}
+
+/// WordPiece trainer that builds vocabulary from corpus.
+pub struct WordPieceTrainer {
+    config: TrainingConfig,
 }
